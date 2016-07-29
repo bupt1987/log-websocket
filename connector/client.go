281cb5163a1e717b5bc4a9cbef7e8ce90f4fd726
俊杰jerry <bupt1987@gmail.com>
2016-07-28 21:56:26 -0500

    SetReadLimit to 1M

--- connector/client.go
+++ connector/client.go
@@ -16,7 +16,7 @@ const (
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
 	// Maximum message size allowed from peer.
-	maxMessageSize = 512
+	maxMessageSize = 1024 * 1024
 )
 
 type Client struct {
